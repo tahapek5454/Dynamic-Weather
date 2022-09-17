0500323d229eee7542f4ea529b4613091148848b
Taha Pek <tahapek5454@gmail.com>
2022-09-18 00:16:38 +0300

    update

--- scripts.js
+++ scripts.js
@@ -5,38 +5,39 @@ const url = "https://api.openweathermap.org/data/2.5/"
 const key = "bca1671cecf44178a4604c8e30cd54ec"
 
 
-let searchBar = document.querySelector('#searchBar')
-searchBar.addEventListener('keypress', setQuery)
 
-function setQuery(e) {
-    if (e.keyCode == '13') // enter
-    {
-        getResult(searchBar.value)
-    }
-}
-const getResult = (citiyName) => {
-    let query = `${url}weather?q=${citiyName}&appid=${key}&units=metric&lang=tr`
-    fetch(query)
-    .then(weather => {
-        return weather.json()
-    })
-    .then(displayResult)
-}
+// let searchBar = document.querySelector('#searchBar')
+// searchBar.addEventListener('keypress', setQuery)
 
-function displayResult(weather){
+// function setQuery(e) {
+//     if (e.keyCode == '13') // enter
+//     {
+//         getResult(searchBar.value)
+//     }
+// }
+// const getResult = (citiyName) => {
+//     let query = `${url}weather?q=${citiyName}&appid=${key}&units=metric&lang=tr`
+//     fetch(query)
+//     .then(weather => {
+//         return weather.json()
+//     })
+//     .then(displayResult)
+// }
 
-    console.log(weather)
+// function displayResult(weather){
 
-    let city = document.querySelector('.city')
-    city.innerHTML=`${weather.name}`
+//     console.log(weather)
 
-    let temp = document.querySelector('.temp')
-    temp.innerHTML=`${Math.round(weather.main.temp)}'C`
+//     let city = document.querySelector('.city')
+//     city.innerHTML=`${weather.name}`
 
-    let desc = document.querySelector('.desc')
-    desc.innerHTML=`${weather.weather[0].description}`
+//     let temp = document.querySelector('.temp')
+//     temp.innerHTML=`${Math.round(weather.main.temp)}'C`
 
-    let min_max = document.querySelector('.min-max')
-    min_max.innerHTML=`${Math.round(weather.main.temp_min)}'C/${Math.round(weather.main.temp_max)}'C`
+//     let desc = document.querySelector('.desc')
+//     desc.innerHTML=`${weather.weather[0].description}`
 
-}
+//     let min_max = document.querySelector('.min-max')
+//     min_max.innerHTML=`${Math.round(weather.main.temp_min)}'C/${Math.round(weather.main.temp_max)}'C`
+
+// }
